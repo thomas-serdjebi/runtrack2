7d64b6b86fbaab6e9b0c7400e684dcdd2b36fe82
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-02 11:08:46 +0100

    jour01/job01 terminé

---
+++ jour01/job01/index.php
@@ -0,0 +1,19 @@
+<?php 
+$str = "LaPlateforme" ;
+$str2 = "Vive";
+$str3 ="!";
+$val = "6";
+$val = $val + 4;
+$myBool = false ;
+
+
+echo $str2;
+echo "&nbsp";
+echo $str;
+echo $str3;
+echo $val;
+echo $myBool;
+
+
+
+?>
