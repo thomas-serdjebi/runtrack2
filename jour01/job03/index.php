24b030b5c6afe0b5c5da10a6d71f909db38baafb
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-02 11:53:24 +0100

    structure HTML

---
+++ jour01/job03/index.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+
+
+
+
+
+
+?>
+
+<html>
+
+<table>
+    <thead>
+        <tr>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+
+</table>
+
+
+
+
+
+
+</html>
