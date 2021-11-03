03bbf60e3754e560e8e44038ee7abfb0b36f0282
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-03 11:38:18 +0100

    job02 changement === pour vérif type et valeur

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -1,6 +1,6 @@
 <?php
 for ($var = 0; $var < 1338; $var=$var+1) {
-    if ($var== 26 || $var === 27 || $var === 88 || $var === 1111 )
+    if ($var=== 26 || $var === 27 || $var === 88 || $var === 1111 )
     {
 
     }
