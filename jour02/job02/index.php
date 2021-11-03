c6c85a296a86125a9dad7f1b549e73ac8ffee383
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-03 10:24:35 +0100

    création rn2/jour02/job02

---
+++ jour02/job02/index.php
@@ -0,0 +1,13 @@
+<?php
+    $var = 0;
+    while( $var < 1337 ) {
+        echo $var = $var + 1 ;
+        echo "<br/>";
+        if ($var == 42) {
+            echo '<b> <u> 42 </b> </u> <br/>';
+            
+        }
+    }
+
+?>
+    
