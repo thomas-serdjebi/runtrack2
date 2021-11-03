f91a2fac5137abc48a9714f3ec0dbd143a0d99ca
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-03 10:21:49 +0100

    rt2/jour02/job01 terminé

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,6 +1,13 @@
 <?php
     $var = 0;
-    while( $var < 1338 ) {
+    while( $var < 1337 ) {
         echo $var = $var + 1 ;
+        echo "<br/>";
+        if ($var == 42) {
+            echo '<b> <u> 42 </b> </u> <br/>';
+            
+        }
     }
- ?>
+
+?>
+    
