5641af930e3eea50e7345377f564a72860faedac
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-11-12 22:32:10 +0100

    job06 ok

---
+++ jour09/job06/index.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "localhost";
+$username = "root" ;
+$password = "" ;
+$dbname = "jour08";
+$tablename = "etudiant";
+
+$con=mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con, "SELECT count(*) as total from etudiant");
+$data = mysqli_fetch_assoc($result) ;
+    echo "<table border='1'>
+    <tr>
+    <th>nombre<th>
+    </tr>";
+    echo "<tbody>";
+    echo "<tr>";
+    echo "<td>" . $data['total'] . "</td>" ;
+    echo "</tr>";
+    echo "</tbody>";
+    echo "</table>";
+
+
+
+
+
+
+
+?>
