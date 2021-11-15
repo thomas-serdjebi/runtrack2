fc47084fdba341808389bd2a83be9650cd1682af
thomas-serdjebi <91789828+thomas-serdjebi@users.noreply.github.com>
2021-11-15 13:27:45 +0100

    ajout manuel job13

--- jour09/job13/index.php
+++ jour09/job13/index.php
@@ -4,6 +4,8 @@ $servername = "localhost";
 $username = "root" ;
 $password = "" ;
 $dbname = "jour08";
+$tablename = "salles";
+$tablename2 = "etage";
 
 $con=mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
@@ -12,25 +14,23 @@ if (mysqli_connect_errno())
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$result1 = mysqli_query($con,"SELECT nom  FROM salles WHERE salles" );
-$result2 = mysqli_query($con,"SELECT nom  FROM etage");
+$result = mysqli_query($con, "SELECT salles.nom as salles_nom , etage.nom as etage_nom FROM $tablename , $tablename2 WHERE salles.id_etage = etage.id");
 
-echo "<table border='1'>
-<tr>
-<th>nom salle</th>
-<th>nom étage</th>
-</tr>";
+    echo "<table border='1'>
+    <tr>
+    <th>nom_salle</th>
+    <th>nom_etage</th>
+    </tr>";
 
-while($row1 = mysqli_fetch_array($result1) && $row2 = mysqli_fetch_array($result2))
-{
-echo "<tbody>";
-echo "<tr>";
-echo "<td>" . $row1['nom'] . "</td>";
-echo "<td>" . $row2['nom'] . "</td>";
-echo "</tr>";
-echo "</tbody>";
-}
-echo "</table>";
+    while($row = mysqli_fetch_array($result))
+    {
+    echo "<tbody>";
+    echo "<tr>";
+    echo "<td>" . $row['salles_nom'] . "</td>" ;
+    echo "<td>" . $row['etage_nom'] . "</td>" ;
+    echo "</tr>";
+    echo "</tbody>";
+    }
+    echo "</table>";
 
-mysqli_close($con);
-?>
+    mysqli_close($con);
